7fae230237ba3dd16896f7d199106c790feb72cf
loopieb <42410284+loopieb@users.noreply.github.com>
2020-04-16 14:40:53 +0100

    Update widget.js

--- apps/widdate/widget.js
+++ apps/widdate/widget.js
@@ -4,11 +4,11 @@
     g.reset();
     g.setFontAlign(0,0); 
     g.setFont("6x8",3);
-      var temp = E.getTemperature()+5.56+"C";
+      var temp = E.getTemperature()+5.56;
        
     g.drawString(temp, this.x+width/2+9, this.y+3);
   }
- g.setColor(0, 0, 0);
+ //g.setColor(0, 0, 0);
   setInterval(function() {
     WIDGETS['temp'].draw(WIDGETS['temp']);
   }, 15000);
