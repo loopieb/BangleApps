3eee02780a14cba319873ced4c20a358168a50ed
loopieb <42410284+loopieb@users.noreply.github.com>
2020-04-17 17:57:39 +0100

    Update clock-analog.js

--- apps/aclock/clock-analog.js
+++ apps/aclock/clock-analog.js
@@ -96,7 +96,7 @@ const drawDate = () => {
   const t = centerY + 37;
   const d = centerY - 37;
   g.drawString(dateDisplay, l, t, true);
-  g.drawString(E.getTemperature()+3.9, l, d, true);
+  g.drawString(E.getTemperature()+3.1, l, d, true);
   // console.log(l, t);
 };
 const onMinute = () => {
