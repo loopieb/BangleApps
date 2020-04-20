e2461dc833b10d33af01376ca5d108602d9c6bd7
loopieb <42410284+loopieb@users.noreply.github.com>
2020-04-20 22:33:07 +0100

    Update widget.js

--- apps/aptwidg/widget.js
+++ apps/aptwidg/widget.js
@@ -2,7 +2,7 @@
   var width = 25; // width of the widget
 var a = [37,37,37,37,37,37,37,37];
   function draw() {
-  var temp = new Temp();
+  var date = new Date();
     g.reset(); // reset the graphics context to defaults (color/font/etc)
     g.setFontAlign(0,0); // center fonts    
        
@@ -19,11 +19,11 @@ var a = [37,37,37,37,37,37,37,37];
     g.drawString(b+"C", this.x+width+4, this.y+18);
   }
   setInterval(function() {
-    WIDGETS["Temp"].draw(WIDGETS["Temp"]);
+    WIDGETS["date"].draw(WIDGETS["date"]);
   }, 60*1000); // update every minute
 
   // add your widget
-  WIDGETS["Temp"]={
+  WIDGETS["date"]={
     area:"tl", // tl (top left), tr (top right), bl (bottom left), br (bottom right)
     width: width, // how wide is the widget?
     draw:draw // called to draw the widget
