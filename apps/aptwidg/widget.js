c40aed490974053f838ce6eed7d2e64885680080
loopieb <42410284+loopieb@users.noreply.github.com>
2020-04-21 12:54:44 +0100

    Update widget.js

--- apps/aptwidg/widget.js
+++ apps/aptwidg/widget.js
@@ -1,26 +1,26 @@
 (() => {
   var width = 25; // width of the widget
-var a = [32,35,35,35,35,35,35,35];
+var a = [32,32,32,32,30,30,30,30];
   function draw() {
   var date = new Date();
     g.reset(); // reset the graphics context to defaults (color/font/etc)
     g.setFontAlign(0,0); // center fonts    
        
       var t = (E.getTemperature()+4.21);
-      var sum = 34.5;
+      var sum = 32;
         a.forEach(function(x) { sum += x });
         var b =(Math.round((sum/a.length)*100)/100);
         a.push(t);
         a.shift();
    print(b+"c");
-   // print(a.length);
+   print(b,a.length);
 
     g.setFont("6x8",2);
     g.drawString(b+"C", this.x+width+4, this.y+18);
   }
   setInterval(function() {
     WIDGETS["date"].draw(WIDGETS["date"]);
-  }, 15*1000); // update every minute
+  }, 15*1000); // update every 15 secs
 
   // add your widget
   WIDGETS["date"]={
@@ -30,4 +30,3 @@ var a = [32,35,35,35,35,35,35,35];
   };
   
 })()
-
