8bb3eefc8354fb6f3fb9a97c0f18f051acd5069d
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-09 21:19:18 +0100

    Update widget.js

--- apps/aptmwid/widget.js
+++ apps/aptmwid/widget.js
@@ -6,14 +6,8 @@
   //g.reset();
    function draw() {
      var date = new Date();
-     //var x = 5;
-     var sum;
-//    g.clear();
-    g.reset(); 
+     g.reset(); 
     g.setFontAlign(0,0); // center fonts
-     // var t = (E.getTemperature());
-      //var sum = 30;
-//var currentTemp = 0;
   function getTemp() { 
 //  var d = 0;  
   NRF.findDevices(function(devices) {
@@ -21,14 +15,13 @@
     for (var i in devices) {
       if (devices[i].name!="MedsTemp") continue;
       // index of 0x1809 in advertised data
-      //print("Found");
       var d = E.toString(devices[i].data);
       var idx = d.indexOf(String.fromCharCode(0x09,0x18));
       if (idx>=0) {
         t = d.charCodeAt(idx+2);
       //  print(t);
         g.setFont("6x8",2.5);
-       g.drawString("M"+t+"c", this.x+width+33, this.y+5);
+       g.drawString("M"+t+"c", this.x+width/2, this.y+12);
       
  // g.flip();
         }
@@ -41,12 +34,10 @@ setInterval(getTemp, 20000);
   }
   setInterval(function() {
  WIDGETS.date.draw(WIDGETS.date);
-   // print("hi");
   }, 30*1000); // update every 30 secs
 
   // add your widget
   WIDGETS.date={
     area:"bl", width: width, draw:draw // called to draw the widget
   }; 
-  
-})();
+})()
