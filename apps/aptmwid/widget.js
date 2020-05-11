47cc168169a01092f79d7fac2a1545142bf5f60a
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-12 00:58:03 +0100

    Update widget.js

--- apps/aptmwid/widget.js
+++ apps/aptmwid/widget.js
@@ -30,6 +30,6 @@ setInterval(getTemp, 20000);
 
   // add your widget
   WIDGETS.date={
-    area:"tl", width: width, draw:draw // called to draw the widget
+    area:"br", width: width, draw:draw // called to draw the widget
   }; 
 })()
