9fda9b1049646a6f4eebc9efd8d5d6c5d0a28524
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-12 01:06:35 +0100

    Update widget.js

--- apps/aptmwid/widget.js
+++ apps/aptmwid/widget.js
@@ -3,9 +3,10 @@
   var width = 24; // width of the widget
    function draw() {
      var temp  = new Temp();
+     var t;
     g.reset(); 
     g.setFontAlign(0,0); // center fonts
-  function getTemp() { 
+  function getTemp(t) { 
   NRF.findDevices(function(devices) {
     var found = false;
     for (var i in devices) {
@@ -14,9 +15,9 @@
       var d = E.toString(devices[i].data);
       var idx = d.indexOf(String.fromCharCode(0x09,0x18));
       if (idx>=0) {
-        t = d.charCodeAt(idx+2);      
-        g.setFont("6x8",2.5);
-       g.drawString("M"+t+"c", this.x+width/2+4, this.y+14);
+   var t = d.charCodeAt(idx+2);      
+    //    g.setFont("6x8",2.5);
+    //    g.drawString("M"+t+"c", this.x+width/2+4, this.y+14);
       }
     }
    }, 2000 /* receive for 2000ms */);
@@ -24,6 +25,8 @@
 // look once a minute for temperature
 setInterval(getTemp, 20000);
    }
+   g.setFont("6x8",2.5);
+       g.drawString("M"+t+"c", this.x+width/2+4, this.y+14);
   setInterval(function() {
  WIDGETS.date.draw(WIDGETS.date);
   }, 10*1000); // update every 30 secs
