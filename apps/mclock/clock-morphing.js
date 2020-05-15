7e9d81731bf6fbc6c01079ed86f738d9361cad75
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-15 12:16:29 +0100

    Update clock-morphing.js

--- apps/mclock/clock-morphing.js
+++ apps/mclock/clock-morphing.js
@@ -150,6 +150,28 @@ function draw(lastText,thisText,n) {
   buf.drawString(date, buf.getWidth()/2, y+8);
   flip();
 }
+function getTemp() { 
+ // var d =0; 
+ g.reset();
+ //g.clear();
+  NRF.findDevices(function(devices) {
+    var found = false;
+    for (var i in devices) {
+      if (devices[i].name!="MedsTemp") continue;
+      // index of 0x1809 in advertised data
+      var d = E.toString(devices[i].data);
+      var idx = d.indexOf(String.fromCharCode(0x09,0x18));
+      if (idx>=0) {
+        t = d.charCodeAt(idx+2);
+        print(t);
+        g.setFont("6x8",2.5);
+       g.drawString("Meds "+t+" c", g.getWidth()/2,    g.getHeight()/2);
+         }
+    }
+   }, 2000 /* receive for 3000ms */);
+ }
+// look once a minute for temperature
+setInterval(getTemp, 20000);
 
 
 
