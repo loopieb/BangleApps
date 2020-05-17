eaf395e577a50237b0c2f320a8b8dd880309d9ee
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-17 22:31:28 +0100

    Update clock-analog.js

--- apps/aclock/clock-analog.js
+++ apps/aclock/clock-analog.js
@@ -43,28 +43,7 @@ let currentDate = new Date();
 const centerX = g.getWidth() / 2;
 const centerY = (g.getWidth() / 2) + widgetHeight/2;
 
-function getTemp() { 
- // var d =0; 
- g.reset();
- //g.clear();
-  NRF.findDevices(function(devices) {
-    var found = false;
-    for (var i in devices) {
-      if (devices[i].name!="MedsTemp") continue;
-      // index of 0x1809 in advertised data
-      var d = E.toString(devices[i].data);
-      var idx = d.indexOf(String.fromCharCode(0x09,0x18));
-      if (idx>=0) {
-        t = d.charCodeAt(idx+2);
-        print(t);
-        g.setFont("6x8",2.5);
-       g.drawString("Meds "+t+" c", g.getWidth()/2,    g.getHeight()/2-10);
-         }
-    }
-   }, 2000 /* receive for 3000ms */);
- }
-// look once a minute for temperature
-setInterval(getTemp, 20000);
+
 
 
 const seconds = (angle) => {
