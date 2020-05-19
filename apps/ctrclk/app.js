41f352d1094a97aa34b5641f4d270969c567023f
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-19 16:40:33 +0100

    Update app.js

--- apps/ctrclk/app.js
+++ apps/ctrclk/app.js
@@ -66,7 +66,7 @@ function drawDate (str) {
   let maxSegmentWidth = 236;
   g.setColor(0.5, 0.5, 0.5);
   g.setColor(0.5, 0.5, 0.5);
-  g.drawString(str, (maxSegmentWidth) - (g.stringWidth(str)), middleY-12,
+  g.drawString(str, (maxSegmentWidth) - (g.stringWidth(str)), middleY-8,
     false);
 }
 
