2d6a6ee3b85c4d60a89cae9ed9857279b5a64b14
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-19 10:43:15 +0100

    Update clock-morphing.js

--- apps/mclock/clock-morphing.js
+++ apps/mclock/clock-morphing.js
@@ -27,7 +27,7 @@ function getTemp() {
         t = d.charCodeAt(idx-2);
        print(t);
         g.setFont("Vector",20);
-       g.drawString("Meds "+t+" c", wt-50, ht+55);
+       g.drawString("Meds "+t+" c", wt-30, ht+55);
          }
     //  g.flip();
     }
