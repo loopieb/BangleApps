b73d852a30492947e02a8137f1408682438fa3f1
loopieb <42410284+loopieb@users.noreply.github.com>
2020-05-21 11:55:22 +0100

    Update bold_clock.js

--- apps/boldclk/bold_clock.js
+++ apps/boldclk/bold_clock.js
@@ -16,8 +16,8 @@ function getTemp() {
       // print(t);
        g.setFontAlign(0, 0);
         g.setFont("Vector",20);
-       g.drawString(" My c", wt+60, ht-20);
-       g.drawString(" "+t+"c", wt+60, ht+20);
+       g.drawString("My c", wt+60, ht-30);
+       g.drawString(" "+t+"c", wt+60, ht+10);
          }
     //  g.flip();
     }
