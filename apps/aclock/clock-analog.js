c12aa3e124ae3e2603396e8ba8f6f367ca302969
loopieb <42410284+loopieb@users.noreply.github.com>
2020-06-07 14:00:27 +0100

    Update clock-analog.js

--- apps/aclock/clock-analog.js
+++ apps/aclock/clock-analog.js
@@ -17,7 +17,7 @@ setInterval(function getTemp() {
         t = d.charCodeAt(idx+2);       
       // print(t);
         g.setFont("Vector",20);
-       g.drawString(t, wt -60, ht-50,true);
+       g.drawString("M "+t, wt -60, ht-50,true);
     // g.flip();   
       }
     }
