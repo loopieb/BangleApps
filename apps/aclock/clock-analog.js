18623f96357b956239cf8cbf8c79a90ad25e5273
loopieb <42410284+loopieb@users.noreply.github.com>
2020-08-03 15:22:37 +0100

    Update clock-analog.js

--- apps/aclock/clock-analog.js
+++ apps/aclock/clock-analog.js
@@ -94,7 +94,8 @@ const drawDate = () => {
 
   const dayString = locale.dow(currentDate, true);
   // pad left date
-  const dateString = (currentDate.getDate() < 10) ? '0' : '' + currentDate.getDate().toString();
+  // const dateString = (currentDate.getDate() < 10) ? '0' : '' + currentDate.getDate().toString();
+  const dateString = ("0"+currentDate.getDate().toString()).substr(-2);
   const dateDisplay = `${dayString}--${dateString}`;
   // console.log(`${dayString}|${dateString}`);
   // center date
